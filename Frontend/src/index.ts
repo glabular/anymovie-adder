70b378d1967d742044a4d21d7fe9729fb7e0ba88
glabular <glu.max06@gmail.com>
2026-01-16 18:17:19 +0100

    Implemented clear forms button

--- Frontend/src/index.ts
+++ Frontend/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+const titleInput = document.getElementById("movie-title-en") as HTMLInputElement;
+const desctiptionInput = document.getElementById("movie-description") as HTMLInputElement;
+const yearInput = document.getElementById("release-year") as HTMLInputElement;
+
+const checkboxes = document.querySelectorAll(
+    ".checkbox-container input[type='checkbox']"
+) as NodeListOf<HTMLInputElement>;
+
+const clearButton = document.getElementById("clear") as HTMLButtonElement;
+
+clearButton.addEventListener("click", clearInput);
+
+function clearInput() {
+    titleInput.value = "";
+    desctiptionInput.value = "";
+    yearInput.value = "";
+
+    checkboxes.forEach(cb => {
+        cb.checked = false;
+    });
+}
