98908eb51aa61a8d353845a458e72d24298815d7
glabular <glu.max06@gmail.com>
2026-01-18 19:29:01 +0100

    Added add movie DTO

---
+++ AnyMovieAdder.API/DTOs/AddMovieRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace AnyMovieAdder.API.DTOs;
+
+public sealed class AddMovieRequest
+{
+    public string? Title { get; init; }
+
+    public string? Description { get; init; }
+
+    public int ReleaseYear { get; init; }
+
+    public string[]? Categories { get; init; }
+}
