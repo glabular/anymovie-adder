bc3ea3a5b2895ed762225d0ede1cd6cd6ed9a9d3
glabular <glu.max06@gmail.com>
2026-01-19 14:16:03 +0100

    Added API health controller with Live endpoint

---
+++ AnyMovieAdder.API/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnyMovieAdder.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : ControllerBase
+{
+    [HttpGet("live")]
+    public IActionResult Live()
+    {
+        return Ok(new
+        {
+            status = "alive",
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
